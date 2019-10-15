1d98c8e3d3fbac21dfe43e8f58f60941aeda2c4e
dziubas <krzysztofdziubanski@gmail.com>
2019-10-15 20:01:16 +0200

    Updates

---
+++ precache-manifest.5e62b420c33fb21a31c132a2face9cd3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48a402c8c47b66ca6c51695f4d3924b",
+    "url": "/portfolioPage/index.html"
+  },
+  {
+    "revision": "43b697b9628dc796cc3f",
+    "url": "/portfolioPage/static/css/main.6ad5f496.chunk.css"
+  },
+  {
+    "revision": "c1c9f348d3c10c996cc5",
+    "url": "/portfolioPage/static/js/2.7fb47328.chunk.js"
+  },
+  {
+    "revision": "43b697b9628dc796cc3f",
+    "url": "/portfolioPage/static/js/main.3c104019.chunk.js"
+  },
+  {
+    "revision": "7d64315ddb0a740b49bd",
+    "url": "/portfolioPage/static/js/runtime-main.e66ae8a9.js"
+  },
+  {
+    "revision": "68432f31e5602b4f3feb0af587617e46",
+    "url": "/portfolioPage/static/media/KrzysztofDziubanski.68432f31.jpg"
+  },
+  {
+    "revision": "e89a9aaad39eb961ab02619f1b61b31b",
+    "url": "/portfolioPage/static/media/KrzysztofDziubanskiKite.e89a9aaa.jpg"
+  },
+  {
+    "revision": "fffff2d215ab237abdff88c3cfbfe7e8",
+    "url": "/portfolioPage/static/media/img3.fffff2d2.png"
+  },
+  {
+    "revision": "a0d02930b6f072ed06c78cdb9eb6ff0a",
+    "url": "/portfolioPage/static/media/img4.a0d02930.png"
+  },
+  {
+    "revision": "e8071cd222564ab34d39578651de4203",
+    "url": "/portfolioPage/static/media/mountainProject.e8071cd2.png"
+  }
+]);
